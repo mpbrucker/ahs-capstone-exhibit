5ae2af18f6e3df6ce7e5acce3274e1d177cfd4d1
Matt Brucker <mpbrucker@gmail.com>
2020-07-30 18:43:10 -0400

    Pants

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,35 @@
+function buildVisitorRepr(id, el) {
+    console.log('adding person');
+    var headEl = document.createElement('a-cone');
+    headEl.setAttribute('height', 0.5);
+    headEl.setAttribute('radius-bottom', 0.25);
+    // rotate to point towards camera direction
+    headEl.object3D.rotation.x = Math.PI / 2;
+    el.appendChild(headEl);
+
+    // line for pointing
+    var lineEl = document.createElement('a-entity');
+    lineEl.setAttribute('line', {
+        start: { x: 0, y: 0, z: 0 }, 
+        end: {x: 0, y: 0, z: -5},
+        color: 'red',
+        visible: true,
+    });
+    el.appendChild(lineEl);
+
+    // name text
+    var textEl = document.createElement('a-text');
+    textEl.setAttribute('value', id);
+    textEl.setAttribute('align', 'center');
+    textEl.getAttribute('position').y = 0.5;
+    textEl.object3D.rotation.y = Math.PI;
+    el.appendChild(textEl);
+    // make a second to point backwards
+    var textEl2 = textEl.cloneNode();
+    textEl2.getAttribute('position').y = 0.5;
+    el.appendChild(textEl2);
+}
+
 AFRAME.registerComponent('proxtext', {
     init: function() {
         this.cam = document.querySelector('#rig');
@@ -19,6 +51,71 @@ AFRAME.registerComponent('proxtext', {
     },
 
 });
+
+AFRAME.registerComponent('textured', {
+    init: function() {
+        var loader = new THREE.TextureLoader();
+        var texturePath  = document.querySelector("#texture").getAttribute('src');
+        var texture = loader.load(texturePath);
+        console.log(texture);
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set( 400, 400 );        
+        texture.needsUpdate = true;
+        console.log(texture)
+        this.material = new THREE.MeshStandardMaterial({
+            map: texture
+        });
+        this.el.addEventListener('model-loaded', () => this.update());
+    },
+    update: function() {
+        const object = this.el.getObject3D('mesh');
+        const material = this.material;
+        console.log(material);
+        console.log(object);
+
+        if (object) {
+            console.log(texture);
+            object.traverse(function (node) {
+                if (node.isMesh) {
+                    console.log('test');
+                    node.material = material;
+                } 
+                // node.material = material;
+            });    
+        }        
+    }
+  })
+
+// AFRAME.registerComponent('textured', {
+//     init: function() {
+//         this.el.addEventListener('model-loaded', (e) => {
+//             var loader = new THREE.TextureLoader();
+//             var texture = document.querySelector("#texture").getAttribute('src')
+//             var mesh = this.el.getObject3D('mesh');
+//             console.log(mesh.children);
+//             console.log(this);
+//             if (!mesh)
+//                 return;
+//             // load a resource
+//             loader.load( texture,
+//                 // onLoad callback
+//                 (texture) => {
+//                     mesh.children.forEach(function(child) {
+//                         child.material.map = texture;
+//                         // child.material.color = 'FF0000';
+//                         console.log(child.material);
+//                     })
+//                     // const material = new THREE.MeshPhongMaterial({
+//                     //     color: 0xFF0000,    // red (can also use a CSS color string here)
+//                     //     flatShading: true,
+//                     //   });
+//                     // mesh.material = material;
+//                 }
+//             )
+//           })
+//     }
+//   })
 
 AFRAME.registerComponent('clicklink', {
     schema: {
